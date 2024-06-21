631ce8f292bb0921406d3486fe4a42e49c4a7f96
denis <dhuber0988@gmail.com>
2024-06-21 16:21:40 +0200

    pass stage

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,5 @@
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 ;
 app/main.py,6/0/60a964aea4a6760d2f0fe80a7725ed9d569edd6c
+>
+your_server.sh,4/b/4b5e0b8e98877d6017dd8f0dce801465e2ea7af2
--- app/main.py
+++ app/main.py
@@ -1,15 +1,26 @@
-# Uncomment this to pass the first stage
+import logging
 import socket
 
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+logger = logging.getLogger("socket_server")
 
-    # Uncomment this to pass the first stage
-    #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+
+SERVER_HOST = "localhost"
+SERVER_PORT = 4221
+
+OK_RESPONSE = b"HTTP/1.1 200 OK\r\n\r\n"
+
+def main():
+
+    # Create a server socket that listen on localhost at port 4221
+    server_socket = socket.create_server((SERVER_HOST, SERVER_PORT), reuse_port=True)
+
+    while True:
+        # Wait for an incoming connection.
+        logger.info(f"Connecting to {SERVER_HOST} on port {SERVER_PORT}")
+        connection, _ = server_socket.accept()
+        logger.info("Sending ok response to client.")
+        connection.sendall(OK_RESPONSE)
 
 
 if __name__ == "__main__":
