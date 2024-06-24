3e4354b319f00f38cd21ede3a092dddc6ae7299f
denis <dhuber0988@gmail.com>
2024-06-24 15:12:42 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -15,29 +15,44 @@ CRLF = "\r\n"
 SERVER_HOST = "localhost"
 SERVER_PORT = 4221
 
-# Representation Header
-CONTENT_TYPE = "Content-Type: text/plain"
-CONTENT_LENGTH = "Content-Length: "
+# Headers
+CONTENT_TYPE_HEADER = "Content-Type: text/plain"
+CONTENT_LENGTH_HEADER = "Content-Length: "
+USER_AGENT_HEADER = "User-Agent: "
+ACCEPT_HEADER = "Accept: "
 
 
-def construct_response(http_version, status, data):
-    # Construct the HTTP response based the information received.
+def construct_response(http_version: str, status: list, data: str) -> bytes:
+    # Construct the HTTP response based on the information received.
     code, reason = status
     base = f"{http_version} {code} {reason}"
-    content_length = "".join([CONTENT_LENGTH, str(len(data))])
+    content_length = "".join([CONTENT_LENGTH_HEADER, str(len(data))])
     body = ""
 
-    headers = [base, CONTENT_TYPE, content_length, body, data]
+    headers = [base, CONTENT_TYPE_HEADER, content_length, body, data]
     response = CRLF.join([header for header in headers])
     return response.encode()
 
 
-def parse_buffer(request: str):
-    # Split the data into its different component (part 1).
-    header = request.split(CRLF)[0]
-    _, path, http_version = header.split()
+def get_user_agent_and_accept(request) -> tuple[str, str]:
+    # User_Agent_Header and Accept_Header appears and 2 and 3 place.
+    # But for some reason (curl formatting ?), their places are sometime switched.
+    user_agent_header, accept_header = request.split(CRLF)[2], request.split(CRLF)[3]
 
-    path_found, data = parse_path(path)
+    if USER_AGENT_HEADER in user_agent_header:
+        return user_agent_header, accept_header
+
+    user_agent_header, accept_header = accept_header, user_agent_header  # Switching value.
+    return user_agent_header, accept_header
+
+
+def parse_buffer(request: str) -> bytes:
+    # Split the request into its different headers.
+    request_header = request.split(CRLF)[0]
+    user_agent_header, _ = get_user_agent_and_accept(request=request)
+    _, path, http_version = request_header.split()
+
+    path_found, data = parse_path(path=path, headers=[user_agent_header])
     if path_found:
         return construct_response(http_version=http_version, status=status_code.HTTP_200_OK, data=data)
 
@@ -54,6 +69,7 @@ def main():
         logger.info(f"Connecting to {SERVER_HOST} on port {SERVER_PORT}")
         connection, _ = server_socket.accept()
         request = connection.recv(BUFF_SIZE).decode("utf-8")
+        print(f"{request =}")
         response = parse_buffer(request=request)
         logger.info("Sending ok response to client.")
         connection.sendto(response, (SERVER_HOST, SERVER_PORT))
--- app/paths.py
+++ app/paths.py
@@ -4,15 +4,17 @@ TRAILING_SLASH = "/"
 
 BASE_PATH = ""  # Path '/' when trailing is removed -> split("/").
 ECHO_PATH = "echo"
+USER_AGENT_PATH = "user-agent"
 
 
 PATHS = [
     BASE_PATH,
     ECHO_PATH,
+    USER_AGENT_PATH
 ]
 
 
-def parse_path(path):
+def parse_path(path: str, headers: list) -> tuple[bool, str]:
     try:
         _, endpoint, data = path.split(TRAILING_SLASH)
     except ValueError:  # There is no data sent (no data to unpack)
@@ -20,6 +22,13 @@ def parse_path(path):
         data = ""
 
     if endpoint in PATHS:
+
+        if endpoint == USER_AGENT_PATH:
+            # Return the version of the product (User-Agent)
+            user_agent_header = headers[0]
+            _, version = user_agent_header.split(" ")
+            return True, version
+
         return True, data
 
     return False, ""
